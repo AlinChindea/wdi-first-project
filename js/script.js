915b839da5b938e4bae8127e69b1f44450339e11
Alin Chindea <alin.chindea@gmail.com>
2017-12-11 14:42:23 +0000

    timer fixed

--- js/script.js
+++ js/script.js
@@ -2,33 +2,22 @@ $(() => {
 
 
   //TIMER
-  // const $timer = $('#timer');
-  // const $timerScreen = $('.screen');
   const $startTime = $('#reset');
-
-  // const timeRemaining = 120;
   let timerIsRunning = false;
   let timerId = null;
 
-  function startStopTimer(duration, display) {
-    let timer = duration, minutes, seconds;
+  function startStopTimer(duration, $display) {
+    let timer = duration;
     // start the timer if it is NOT running
     if(!timerIsRunning){
       timerId = setInterval(() => {
+        --timer;
         let minutes = parseInt(timer / 60, 10);
         let seconds = parseInt(timer % 60, 10);
-
         minutes = minutes < 10 ? '0' + minutes : minutes;
         seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        display.text(minutes + ':' + seconds);
-        if (--timer < 0) {
-          timer = duration;
-        }
-        // timeRemaining--;
-        // $timerScreen.text(timeRemaining);
-
-        if(timer === 0) {
+        $display.text(minutes + ':' + seconds);
+        if(timer  === 0) {
           clearInterval(timerId);
           // $timer.addClass('ringing');
         }
@@ -37,7 +26,7 @@ $(() => {
     }
   }
   function startTimer() {
-    const twoMinutes = 60 * 2;
+    const twoMinutes = 120;
     const $timerScreen = $('#time');
     startStopTimer(twoMinutes, $timerScreen);
   }
