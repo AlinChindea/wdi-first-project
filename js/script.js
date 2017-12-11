48cc9e9ddc81fccf38c66a0edff6cbf911ccaaed
Alin Chindea <alin.chindea@gmail.com>
2017-12-11 13:08:37 +0000

    draft layout design + timer

--- js/script.js
+++ js/script.js
@@ -1,35 +1,47 @@
-// alert('Loaded JS');
 $(() => {
 
-  const $timer = $('#timer');
-  const $timerScreen = $('.display');
+
+  //TIMER
+  // const $timer = $('#timer');
+  // const $timerScreen = $('.screen');
   const $startTime = $('#reset');
 
-  let timeRemaining = 120;
+  // const timeRemaining = 120;
   let timerIsRunning = false;
   let timerId = null;
 
-  function startStopTimer() {
-    // stop the timer if it is running
-    if(timerIsRunning) {
-      clearInterval(timerId);
-      timerIsRunning = false;
-    } else {
-      // start the timer if it is NOT running
+  function startStopTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    // start the timer if it is NOT running
+    if(!timerIsRunning){
       timerId = setInterval(() => {
-        timeRemaining--;
-        $timerScreen.text(timeRemaining);
+        let minutes = parseInt(timer / 60, 10);
+        let seconds = parseInt(timer % 60, 10);
 
-        if(timeRemaining === 0) {
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        display.text(minutes + ':' + seconds);
+        if (--timer < 0) {
+          timer = duration;
+        }
+        // timeRemaining--;
+        // $timerScreen.text(timeRemaining);
+
+        if(timer === 0) {
           clearInterval(timerId);
-          $timer.addClass('ringing');
+          // $timer.addClass('ringing');
         }
       }, 1000);
       timerIsRunning = true;
     }
   }
-  $startTime.on('click', startStopTimer);
-
+  function startTimer() {
+    const twoMinutes = 60 * 2;
+    const $timerScreen = $('#time');
+    startStopTimer(twoMinutes, $timerScreen);
+  }
+  $startTime.on('click', startTimer);
 
 
 });
